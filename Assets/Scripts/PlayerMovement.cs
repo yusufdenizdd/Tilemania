4cd229aace957c77a403b8f9aa08e59d33b3c450
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2026-09-16 15:07:12 +0300

    Tilemania 11/41

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,15 +7,22 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
 
+    Rigidbody2D myRigidbody;
+
+    [SerializeField] float runSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+        FlipSprite();
 
     }
 
@@ -22,5 +30,19 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
+        myRigidbody.linearVelocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Math.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+        if (hasHorizontalSpeed)
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.linearVelocity.x), 1f);
+
     }
 }
